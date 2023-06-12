1e2b2361549d53b054e638026393626cc91b1b59
Soli <Soli0222@proton.me>
2023-06-13 01:48:20 +0900

    loginからcallbackに飛ばすことに成功

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,27 @@
-export default function login() {
-    return (
-        <main>
-            <p>app/login/page.tsx</p>
-        </main>
-    )
-    
-}
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const LoginPage = () => {
+    const router = useRouter();
+    const authUrl = 'https://accounts.spotify.com/authorize';
+    const scope = 'user-read-currently-playing user-read-playback-state';
+    const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID || '';
+    const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI || '';
+  
+    useEffect(() => {
+      const params = new URLSearchParams({
+        client_id: clientId,
+        response_type: 'code',
+        redirect_uri: redirectUri,
+        scope: scope,
+      });
+  
+      router.push(`${authUrl}?${params.toString()}`);
+    }, []);
+  
+    return null;
+  };
+  
+  export default LoginPage;
